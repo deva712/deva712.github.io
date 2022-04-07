87e274ffb55b7d55c8f7a7d67cd1b21810631532
deva712 <102367957+deva712@users.noreply.github.com>
2022-04-07 17:07:48 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Left Hand Batsmen", "Orthodox Spinner"],
+        strings: ["Student", "Software Developer","Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Allrounder"],
+        strings: ["CSE UNDERGRADUATE"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
